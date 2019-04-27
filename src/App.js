d719258fa224a0ff7df52b7f7b331bd66979601f
zavion sherman <zsherman2510@hotmail.com>
2019-04-26 23:15:25 -0700

    added the header images with carousel effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
-
+import Featured from "./components/featured";
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: "1500px", background: "red" }}>
         <Header />
+        <Featured />
       </div>
     );
   }
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -3,7 +3,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
-
 import SideDrawer from "./SideDrawer";
 
 export default class Header extends Component {
