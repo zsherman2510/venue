5ff5f0aad85dba0656c015db580b4517f79b8928
zavion sherman <zsherman2510@hotmail.com>
2019-04-28 12:04:44 -0700

    finished the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
+import { Element } from "react-scroll";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenueNfo from "./components/venueNFO";
@@ -12,11 +13,23 @@ class App extends Component {
     return (
       <div className="App" style={{ height: "1500px", background: "red" }}>
         <Header />
-        <Featured />
-        <VenueNfo />
-        <Highlights />
-        <Pricing />
-        <Location />
+        <Element name="featured">
+          <Featured />
+        </Element>
+        <Element name="venueNfo">
+          <VenueNfo />
+        </Element>
+
+        <Element name="highlights">
+          <Highlights />
+        </Element>
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="location">
+          <Location />
+        </Element>
+
         <Footer />
       </div>
     );
